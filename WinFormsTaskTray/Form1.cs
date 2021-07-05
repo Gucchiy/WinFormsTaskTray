5bc9124f5e445ca6f3cb8af2c012d5d97584aa8f
Yoshihiro Yamaguchi <gucchiy@gmail.com>
2021-07-05 20:19:58 +0900

    プロジェクト ファイルを追加します。

---
+++ WinFormsTaskTray/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace WinFormsTaskTray
+{
+    public partial class Form1 : Form
+    {
+        NotifyIcon notifyIcon;
+        public Form1()
+        {
+            InitializeComponent();
+            ShowInTaskbar = false;
+            SetComponents();
+            
+        }
+
+        private void SetComponents()
+        {
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = new Icon(@"C:\temp\favicon.ico");
+            notifyIcon.Visible = true;
+            notifyIcon.Text = "NofityIcon のテスト";
+
+            ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
+            ToolStripMenuItem toolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem.Text = "&終了";
+            toolStripMenuItem.Click += ToolStripMenuItem_Click;
+            contextMenuStrip.Items.Add(toolStripMenuItem);
+            notifyIcon.ContextMenuStrip = contextMenuStrip;
+        }
+
+        private void ToolStripMenuItem_Click(object sendor, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
